f06e7467a26bcf20449b0a4ff34aae17e6ec0521
不忘初心 <1624717079@qq.com>
2021-03-25 15:35:23 +0800

    2021-3-25

--- src/org/devops/ding.groovy
+++
@@ -1,7 +0,0 @@
-def message(String source, String add){
-    if(!source){
-        source = ""
-    }
-    env.BUILD_TASKS = source + add + "\n&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
-    return env.BUILD_TASKS
-}
