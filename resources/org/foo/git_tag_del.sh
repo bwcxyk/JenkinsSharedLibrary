163ad4f3557cf7ffe5cdf3ae48d7894f168ee240
不忘初心 <1624717079@qq.com>
2021-05-17 13:49:00 +0800

    2021-5-17

---
+++ resources/org/foo/git_tag_del.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# 删除旧的tag，保留最近三个月。
+# author YaoKun
+#set -x
+
+month=$(date +%Y%m)
+month1=$(date -d "${month}01 last month" +%Y%m)
+month2=$(date -d "${month1}01 last month" +%Y%m)
+
+echo "保留以下月份tag:${month},${month1},${month2}"
+
+#git show-ref --tag | grep -v ${month} | grep -v ${month1} | grep -v ${month2} | awk '{print ":" $2}' > tmp.txt
+
+git show-ref --tag | \
+grep -v ${month} |   \
+grep -v ${month1} |  \
+grep -v ${month2} |  \
+awk '{print ":" $2}' > tmp.txt
+
+file=tmp.txt
+
+for i in `cat ${file}`                
+do
+git push origin $i;
+done;
+rm -f tmp.txt
+
+# 同步远程tag到本地
+git fetch --prune --tags
