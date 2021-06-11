9930420c62c86bf50adda0ad834c9fd6af76827d
不忘初心 <1624717079@qq.com>
2021-06-11 13:24:00 +0800

    精简

--- resources/org/foo/push_1of2.sh
+++ resources/org/foo/push_1of2.sh
@@ -2,40 +2,28 @@
 #食用方法 sh push2.sh ${repo} ${tag}
 
 
-REGISTRY_URL="192.168.0.2" #镜像仓库url
-NAME_SPACE="public" #命名空间
-DATE=$(date +"%Y%m%d%H%M%S")
-TAG_VERSION=${DATE} #镜像的版本
-
-if [ "$2" != "" ];
-    then
-    TAG_VERSION="$2"
-fi
-
 # build_push_tag
 push_tag()
 {
-    sh $WORKSPACE/push.sh ${REGISTRY_URL} ${TAG_VERSION}
+    sh $WORKSPACE/push.sh ${REGISTRY_URL}
 }
 
 if [ "$1" = "local" ];
     then
     # login
     REGISTRY_URL="192.168.0.2"
-    NAME_SPACE="yuanfu-tms"
-    docker login --username=${username} --password=${password} ${REGISTRY_URL}
+    docker login --username=admin ${REGISTRY_URL} --password=Yuanfu1211
     push_tag
     elif [ "$1" = "aliyun" ];
     then
     REGISTRY_URL="registry.cn-shanghai.aliyuncs.com"
-    NAME_SPACE="yuanfu-tms"
-    docker login --username=${username} --password=${password} ${REGISTRY_URL}
+    docker login --username=远孚集团 ${REGISTRY_URL} --password=wzygymtijus9
     push_tag
     elif [ "$1" = "huaweicloud" ];
     then
     REGISTRY_URL="swr.cn-east-2.myhuaweicloud.com"
-    NAME_SPACE="yuanfu-tms"
-    docker login --username=${username} --password=${password} ${REGISTRY_URL}
+    docker login --username=cn-east-2@VMKGNWR86FUNMDMKNPKJ --password=eefd5bebe0e349a5ed2ffb22bf0a488ec8dba5d2a3075272f8f0d79e0f09616e ${REGISTRY_URL}
+    #docker login -u cn-east-2@VMKGNWR86FUNMDMKNPKJ -p eefd5bebe0e349a5ed2ffb22bf0a488ec8dba5d2a3075272f8f0d79e0f09616e ${REGISTRY_URL}
     push_tag
     else
     echo '本地仓库:push.sh local 1.0'
--- resources/org/foo/push_2of2.sh
+++ resources/org/foo/push_2of2.sh
@@ -3,36 +3,23 @@
 
 
 REGISTRY_URL="192.168.0.2" #镜像仓库url
-NAME_SPACE="public" #命名空间
-TAG_NAME="api"
-TAG_NAME2="api-job"
-TAG_NAME3="api-db-update"
-TAG_VERSION="v1.0" #镜像的版本
+NAME_SPACE="yuanfu-wms" #命名空间
+DATE=$(date +"%Y%m%d%H%M%S")
+TAG_VERSION=${DATE} #镜像的版本
+# service
+TAG_NAME="tms"
+TAG_NAME2=""
 
 if [ "$1" != "" ];
     then
     REGISTRY_URL="$1"
 fi
 
-if [ "$2" != "" ];
-    then
-    TAG_VERSION="$2"
-fi
 
 # build_push_tag
 set -e
-# push api
-cd $WORKSPACE/api-server
+# push tms
+cd $WORKSPACE
 docker build -t ${REGISTRY_URL}/${NAME_SPACE}/${TAG_NAME}:${TAG_VERSION} .
 docker push ${REGISTRY_URL}/${NAME_SPACE}/${TAG_NAME}:${TAG_VERSION}
 docker rmi ${REGISTRY_URL}/${NAME_SPACE}/${TAG_NAME}:${TAG_VERSION}
-# push job
-cd $WORKSPACE/api-job
-docker build -t ${REGISTRY_URL}/${NAME_SPACE}/${TAG_NAME2}:${TAG_VERSION} .
-docker push ${REGISTRY_URL}/${NAME_SPACE}/${TAG_NAME2}:${TAG_VERSION}
-docker rmi ${REGISTRY_URL}/${NAME_SPACE}/${TAG_NAME2}:${TAG_VERSION}
-# push db-update
-cd $WORKSPACE/api-db-update
-docker build -t ${REGISTRY_URL}/${NAME_SPACE}/${TAG_NAME3}:${TAG_VERSION} .
-docker push ${REGISTRY_URL}/${NAME_SPACE}/${TAG_NAME3}:${TAG_VERSION}
-docker rmi ${REGISTRY_URL}/${NAME_SPACE}/${TAG_NAME3}:${TAG_VERSION}
