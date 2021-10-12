938e23b94f33cf8b70c961628fb280ed8e54bc09
不忘初心 <1624717079@qq.com>
2021-10-12 11:43:38 +0800

    Update push_1of2.sh

--- resources/org/foo/push_1of2.sh
+++ resources/org/foo/push_1of2.sh
@@ -12,18 +12,18 @@ if [ "$1" = "local" ];
     then
     # login
     REGISTRY_URL="192.168.0.2"
-    docker login --username=admin ${REGISTRY_URL} --password=Yuanfu1211
+    docker login --username=admin ${REGISTRY_URL} --password=admin
     push_tag
     elif [ "$1" = "aliyun" ];
     then
     REGISTRY_URL="registry.cn-shanghai.aliyuncs.com"
-    docker login --username=远孚集团 ${REGISTRY_URL} --password=wzygymtijus9
+    docker login --username=远孚集团 ${REGISTRY_URL} --password=admin
     push_tag
     elif [ "$1" = "huaweicloud" ];
     then
     REGISTRY_URL="swr.cn-east-2.myhuaweicloud.com"
-    docker login --username=cn-east-2@VMKGNWR86FUNMDMKNPKJ --password=eefd5bebe0e349a5ed2ffb22bf0a488ec8dba5d2a3075272f8f0d79e0f09616e ${REGISTRY_URL}
-    #docker login -u cn-east-2@VMKGNWR86FUNMDMKNPKJ -p eefd5bebe0e349a5ed2ffb22bf0a488ec8dba5d2a3075272f8f0d79e0f09616e ${REGISTRY_URL}
+    docker login --username=cn-east-2@VMKGNWR86FUNMDMKNPKJ --password=eefd5bebe0e3dfdsfdsfdfdf0d79e0f09616e ${REGISTRY_URL}
+    #docker login -u cn-east-2@VMKGNWR86FUNMDMKNPKJ -p eefd5bebe0e3dfdsfdsfdfdf0d79e0f09616e ${REGISTRY_URL}
     push_tag
     else
     echo '本地仓库:push.sh local 1.0'
