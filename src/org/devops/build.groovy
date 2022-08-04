3eb500f6d6ed519ff9442bf348113195be440cf3
不忘初心 <1624717079@qq.com>
2022-08-04 15:29:23 +0800

    Update build.groovy

--- src/org/devops/build.groovy
+++ src/org/devops/build.groovy
@@ -9,5 +9,7 @@ def Build(buildType,buildShell){
     println("当前选择的构建类型为 ${buildType}")
     buildHome= tool buildTools[buildType]
     
-    sh "${buildHome}/bin/${buildType} ${buildShell}"
+    //sh "${buildHome}/bin/${buildType} ${buildShell}"
+    sh "${buildType} ${buildShell}"
+
 }
