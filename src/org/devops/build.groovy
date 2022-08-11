ab69bcffe4b49560ab1c61988e3d6190cf09dc33
不忘初心 <1624717079@qq.com>
2022-08-11 16:30:22 +0800

    2022-8-11

--- src/org/devops/build.groovy
+++ src/org/devops/build.groovy
@@ -1,15 +1,14 @@
 package org.devops
 
 
-//构建类型
+// 构建类型
 def Build(buildType,buildShell){
     def buildTools = ["mvn":"M2","ant":"ANT","gradle":"GRADLE","npm":"NPM","cnpm":"NPM"]
     
-    
     println("当前选择的构建类型为 ${buildType}")
-    buildHome= tool buildTools[buildType]
+    // buildHome= tool buildTools[buildType]
     
-    //sh "${buildHome}/bin/${buildType} ${buildShell}"
+    // sh "${buildHome}/bin/${buildType} ${buildShell}"
     sh "${buildType} ${buildShell}"
 
 }
