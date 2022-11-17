41f2f4719eeda2219db58bacfdbc21b3535f0be0
不忘初心 <1624717079@qq.com>
2022-11-17 10:25:06 +0800

    Update push_2of2.sh

--- resources/com/yuanfu/pipeline/scripts/push_2of2.sh
+++ resources/com/yuanfu/pipeline/scripts/push_2of2.sh
@@ -3,18 +3,21 @@
 
 
 REGISTRY_URL="192.168.0.2" #镜像仓库url
-NAME_SPACE="yuanfu-wms" #命名空间
+NAME_SPACE="test" #命名空间
 DATE=$(date +"%Y%m%d%H%M%S")
 TAG_VERSION=${DATE} #镜像的版本
 # service
-TAG_NAME="tms"
-# TAG_NAME2=""
+TAG_NAME="test"
 
 if [ "$1" != "" ];
     then
     REGISTRY_URL="$1"
 fi
 
+if [ "$2" != "" ];
+    then
+    TAG_VERSION="$2"
+fi
 
 # build_push_tag
 set -e
