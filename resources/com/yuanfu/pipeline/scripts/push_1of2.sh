b80aa95452f925c0623849dbd43ad101b80ae431
不忘初心 <1624717079@qq.com>
2023-12-25 11:26:12 +0800

    Update push_1of2.sh

--- resources/com/yuanfu/pipeline/scripts/push_1of2.sh
+++ resources/com/yuanfu/pipeline/scripts/push_1of2.sh
@@ -12,29 +12,26 @@ push_tag()
     sh $WORKSPACE/push_2of2.sh ${REGISTRY_URL} ${TAG_VERSION}
 }
 
-if [ "$1" = "local" ];
-    then
+if [ "$1" = "local" ] || [ -z "$1" ]; then
     # login
     USERNAME=""
     PASSWORD=""
     REGISTRY_URL=""
     echo "${PASSWORD}" | docker login --username=${USERNAME} ${REGISTRY_URL} --password-stdin
     push_tag
-    elif [ "$1" = "aliyun" ];
-    then
+elif [ "$1" = "aliyun" ]; then
     USERNAME=""
     PASSWORD=""
     REGISTRY_URL="registry.cn-shanghai.aliyuncs.com"
     echo "${PASSWORD}" | docker login --username=${USERNAME} ${REGISTRY_URL} --password-stdin
     push_tag
-    elif [ "$1" = "huaweicloud" ];
-    then
+elif [ "$1" = "huaweicloud" ]; then
     USERNAME=""
     PASSWORD=""
     REGISTRY_URL="swr.cn-east-2.myhuaweicloud.com"
     echo "${PASSWORD}" | docker login --username=${USERNAME} ${REGISTRY_URL} --password-stdin
     push_tag
-    else
+else
     echo '本地仓库:push.sh local 1.0'
     echo '阿里云仓库:push.sh aliyun 1.0'
     echo '华为云仓库:push.sh huaweicloud 1.0'
