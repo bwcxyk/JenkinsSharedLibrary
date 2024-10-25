635d50c685f6ae70fea454111e9a84b0a5ec7cc6
不忘初心 <1624717079@qq.com>
2024-10-25 13:45:39 +0800

    Update maven.groovy

--- src/org/devops/maven.groovy
+++ src/org/devops/maven.groovy
@@ -25,10 +25,10 @@ def readSettingsXml() {
     writeFile file: 'settings.xml', text: settingsContent
 }
 
-def test(additionalMavenArgs = "") {
+def test(additionalArgs = "") {
     readSettingsXml()
 
-    def mvnCommand = "mvn -s settings.xml test ${additionalMavenArgs}"
+    def mvnCommand = "mvn -s settings.xml test ${additionalArgs}"
     def exitCode = sh(script: mvnCommand, returnStatus: true)
 
     if (exitCode != 0) {
@@ -36,10 +36,10 @@ def test(additionalMavenArgs = "") {
     }
 }
 
-def build(additionalMavenArgs = "") {
+def build(additionalArgs = "") {
     readSettingsXml()
 
-    def mvnCommand = "mvn -s settings.xml clean package -Dmaven.test.skip=true ${additionalMavenArgs}"
+    def mvnCommand = "mvn -s settings.xml clean package -Dmaven.test.skip=true ${additionalArgs}"
     def exitCode = sh(script: mvnCommand, returnStatus: true)
 
     if (exitCode != 0) {
@@ -47,10 +47,10 @@ def build(additionalMavenArgs = "") {
     }
 }
 
-def deploy(additionalMavenArgs = "") {
+def deploy(additionalArgs = "") {
     readSettingsXml()
 
-    def mvnCommand = "mvn -s settings.xml clean deploy -Dmaven.test.skip=true ${additionalMavenArgs}"
+    def mvnCommand = "mvn -s settings.xml clean deploy -Dmaven.test.skip=true ${additionalArgs}"
     def exitCode = sh(script: mvnCommand, returnStatus: true)
 
     if (exitCode != 0) {
