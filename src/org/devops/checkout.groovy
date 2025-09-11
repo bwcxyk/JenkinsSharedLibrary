27f16ca8a8e151d0616d8a3fb9c4f6cf7cfd1928
不忘初心 <1624717079@qq.com>
2025-09-11 13:20:57 +0800

    Update checkout.groovy

--- src/org/devops/checkout.groovy
+++ src/org/devops/checkout.groovy
@@ -1,20 +1,29 @@
 package org.devops
 
-def call(){
-  checkout scm
-}
+class Checkout implements Serializable {
+    def call(){
+        checkout scm
+    }
+    
+    def checkoutCustom(){
+        checkout([
+            $class: 'GitSCM',
+            branches: scm.branches,
+            doGenerateSubmoduleConfigurations: scm.doGenerateSubmoduleConfigurations,
+            extensions: [
+                // 设置 clone 参数
+                [$class: 'CloneOption', depth: 1, noTags: false],
+                // 添加 GitLFSPull 插件
+                [$class: 'GitLFSPull']
+            ],
+            userRemoteConfigs: scm.userRemoteConfigs
+        ])
+    }
 
-def checkoutCustom(){
-    checkout([
-        $class: 'GitSCM',
-        branches: scm.branches,
-        doGenerateSubmoduleConfigurations: scm.doGenerateSubmoduleConfigurations,
-        extensions: [
-            // 设置 clone 参数
-            [$class: 'CloneOption', depth: 1, noTags: false],
-            // 添加 GitLFSPull 插件
-            [$class: 'GitLFSPull']
-        ],
-        userRemoteConfigs: scm.userRemoteConfigs
-    ])
+    def printNodeInfo(){
+        echo "===== Jenkins Info ====="
+        echo "Jenkins Node Name: ${env.K8S_NODE_NAME}"  // 节点名称
+        echo "Jenkins Node IP: ${env.K8S_NODE_IP}"      // 节点 IP
+        echo "=============================="
+    }
 }
