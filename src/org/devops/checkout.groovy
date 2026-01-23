574a6afa91928bb4b298bd90caed7370db72fbe6
不忘初心 <1624717079@qq.com>
2026-01-23 09:18:13 +0800

    Add node info

--- src/org/devops/checkout.groovy
+++ src/org/devops/checkout.groovy
@@ -22,18 +22,10 @@ def checkoutCustom(){
 }
 
 def printNodeInfo(){
-    def nodeName = sh(
-        script: 'set +x; printf "%s" "$K8S_NODE_NAME"',
-        returnStdout: true
-    ).trim()
-
-    def nodeIp = sh(
-        script: 'set +x; printf "%s" "$K8S_NODE_IP"',
-        returnStdout: true
-    ).trim()
-
+    // 只有 JNLP Container 可以传递系统环境变量到 Jenkins
+    // 其他 Container 使用无法获取到
     echo "===== Jenkins Info ====="
-    echo "Jenkins Node Name: ${nodeName}"   // 节点名称
-    echo "Jenkins Node IP: ${nodeIp}"       // 节点 IP
+    echo "Jenkins Node Name: ${env.K8S_NODE_NAME}"  // 节点名称
+    echo "Jenkins Node IP: ${env.K8S_NODE_IP}"      // 节点 IP
     echo "=============================="
 }
